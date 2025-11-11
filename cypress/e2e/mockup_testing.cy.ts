7f97d41501c2fb1430f0e935ebde1e72c78d6ebf
JaPataki <pataki1305@gmail.com>
2025-11-11 10:10:14 +0100

    added idk

--- cypress/e2e/mockup_testing.cy.ts
+++ cypress/e2e/mockup_testing.cy.ts
@@ -1,19 +1,55 @@
 describe("Home Page Albums", () => {
-  it("intercepts and mocks API", () => {
+  it("Expected statusCode 500", () => {
     // 1️⃣ Zachytenie requestu
     cy.intercept("GET", "/api/albums", {
-      statusCode: 200,
-      body: [{ id: 1, name: "Mocked Album", author_name: "Mock Artist", release_date: "11-11-2025" }]
-      
+      statusCode: 500,
+      body: [{ id: 1, name: "Mocked Album", author_name: "Mock Artist", release_date: "11/11/2025" }]
+
     }).as("getAlbums");
 
     // 2️⃣ Návšteva stránky
     cy.visit("/");
 
-    // 3️⃣ Počkaj, kým sa API zavolá
+    
     cy.wait("@getAlbums");
 
-    // 4️⃣ Over, že UI zobrazilo výsledok
-    cy.contains("Mocked Album").should("be.visible");
+    
+    cy.contains("Truhlik nemaš server !").should("be.visible");
+   });
+
+  it("Shows loading state", () => {
+   cy.intercept("GET", "/api/albums", (req) => {
+      req.on("response", (res) => {
+        res.setDelay(2000); 
+      });
+    }).as("getAlbums");
+
+ 
+    cy.visit("/");
+    
+    cy.contains("Loading...").should("be.visible"); 
+  
+  
+  
   });
+
+  it("Loading stage disapiers when data is loaded", () => {
+    cy.intercept("GET", "/api/albums", (req) => {
+      req.on("response", (res) => {
+        res.setDelay(2000); 
+      });
+    }).as("getAlbums");
+
+   
+    cy.visit("/");
+   
+    cy.wait("@getAlbums");
+
+   
+    cy.contains("Loading...").should("not.exist"); 
+  });
+
+  
+
 });
+
